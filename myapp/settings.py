8afe7ccc06a6e9e41a0e7a7800009b2d81099168
ncip48 <mbahcip00@gmail.com>
2024-06-06 10:23:20 +0700

    add datatables

--- myapp/settings.py
+++ myapp/settings.py
@@ -132,6 +132,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
